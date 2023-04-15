f31abc643c28113daee61456a6ec38837d378988
Yoann Coldefy <yoann.coldefy@gmail.com>
2023-04-15 23:54:20 +0900

    move package.swift to root, correct settings

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "Beethoven",
+    platforms: [.iOS(.v13)],
+    products: [.library(name: "Beethoven", targets: ["Beethoven"])],
+    targets: [
+        .target(name: "Beethoven"),
+        .testTarget(name: "BeethovenTests",
+                    dependencies: ["Beethoven"],
+                    resources: [.copy("TestResources/")]
+                   ),
+    ]
+}
