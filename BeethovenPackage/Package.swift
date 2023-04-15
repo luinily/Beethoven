0c1a35dade21db58c81ef8f7ce4f850e0cb5339d
Yoann Coldefy <yoann.coldefy@gmail.com>
2023-04-15 23:28:21 +0900

    name changes

--- BeethovenPackage/Package.swift
+++ BeethovenPackage/Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "BeethovenPackage",
+    name: "Beethoven",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BeethovenPackage",
-            targets: ["BeethovenPackage"]),
+            name: "Beethoven",
+            targets: ["Beethoven"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "BeethovenPackage",
+            name: "Beethoven",
             dependencies: []),
         .testTarget(
-            name: "BeethovenPackageTests",
-            dependencies: ["BeethovenPackage"]),
+            name: "BeethovenTests",
+            dependencies: ["Beethoven"]),
     ]
 )
--- BeethovenPackage/Tests/BeethovenTests/BeethovenTests.swift
+++ BeethovenPackage/Tests/BeethovenTests/BeethovenTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import BeethovenPackage
 
-final class BeethovenPackageTests: XCTestCase {
+final class BeethovenTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
